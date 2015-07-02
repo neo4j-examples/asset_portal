370f355437a2c03a064190569e1387b282f3802b
Brian Underwood <ml+github@semi-sentient.com>
2015-07-02 17:19:00 -0600

    Properties from episode 2

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,7 +1,31 @@
+class RationalConverter
+  class << self
+    def primitive_type; String; end
+
+    def convert_type; Rational; end
+
+    def to_db(value)
+      "#{value.numerator}/#{value.denominator}"
+    end
+
+    def to_ruby(value)
+      value && Rational(value)
+    end
+    alias_method :call, :to_ruby
+  end
+  include Neo4j::Shared::Typecaster
+end
+
 class Asset 
   include Neo4j::ActiveNode
-  property :title, type: String
-  property :created_at, type: DateTime
-  property :updated_at, type: DateTime
 
+  id_property :code
+  property :title, type: Integer
+  property :price, type: Rational
+
+  property :details
+  serialize :details
+
+  property :created_at
+  property :updated_at
 end
