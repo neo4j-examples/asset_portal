d4427a3008d986f001bec1d8afa3afb474ca3ba3
Brian Underwood <ml+github@semi-sentient.com>
2015-07-19 18:36:41 -0600

    RSpec and Rubocop

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,16 @@
 require File.expand_path('../boot', __FILE__)
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
 # require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'sprockets/railtie'
 require 'neo4j/railtie'
-require "rails/test_unit/railtie"
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,9 +18,8 @@ Bundler.require(*Rails.groups)
 
 module AssetPortal
   class Application < Rails::Application
-    
     config.generators do |g|
-      g.orm             :neo4j
+      g.orm :neo4j
     end
 
     # Configure where the embedded neo4j database should exist
